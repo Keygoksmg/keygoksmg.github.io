0547c7730f4e5ba84add100fec2a0e1a7d125396
Keigo Kusumegi <kusukun7@gmail.com>
2025-01-28 08:49:05 -0500

    update socials and GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
     // github pages
     site: 'https://keygoksmg.github.io/',
     // base: '/keygoksmg.github.io',
 
-    // partytown settings
-    integrations: [], // partytown({ config: { forward: ["dataLayer.push"] } })
+    // integration settings
+    integrations: [
+      icon(), 
+      partytown({ config: { forward: ["dataLayer.push"] } })
+    ],
+
+    // to prevent from getting hot reload stoping after importing third-party scripts 
+    vite: {
+      server: {
+        watch: {
+          usePolling: true,
+        },
+      },
+    },
   });
